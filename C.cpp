b2316236357fd3282e99934cf77b107fa6faca8b
xu <2556259859@qq.com>
2020-10-30 11:33:36 +0800

    xu 添加一个新的排序--插入排序

--- C.cpp
+++ C.cpp
@@ -1,6 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int insertSort(int a[], int len){
+	for(int i = 1; i < len; i++){
+		int j,temp = a[i];
+		for(j = i-1; j >=0; j--){
+			if(temp < a[j]){
+				a[j+1] = a[j];
+			}else{
+				break;
+			}
+		}
+		a[j+1] = temp;
+	}
+}
+
+
 int quickSort(int a[],int low,int high){
 	if(low < high){
 		int l = low, h =high,mid;
@@ -22,7 +37,7 @@ int quickSort(int a[],int low,int high){
 
 int main(){
 	int a[10] = {3,5,7,9,2,4,8,2,0,22};
-	quickSort(a,0,9);
+	insertSort(a,10);
 	for(int i = 0; i < 10; i++){
 		printf("%d ",a[i]);
 	} 
